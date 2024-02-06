bab7804a5eea92a6fe88bcd186fa54f0a1abf9f9
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2024-02-05 22:27:22 -0300

    projeto inicial de agenda

---
+++ App.GRRInnovations.AgendaPro/App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace App.GRRInnovations.AgendaPro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App.GRRInnovations.AgendaPro/App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace App.GRRInnovations.AgendaPro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
